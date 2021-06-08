5f40fec58429b8bc1ab6222932d67e7103a9035e
Varun Nayak <varunnayak@Varuns-MacBook-Pro.local>
2021-06-08 14:08:05 -0500

    Added todo functionality

--- app.js
+++ app.js
@@ -6,5 +6,27 @@ const todoList= document.querySelector('.todo-list');
 todoButton.addEventListener('click', addTodo);
 //Functions
 function addTodo(event) {
-    console.log("hello");
+    // Prevent form from submitting
+    event.preventDefault();
+    // Todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo')
+    // Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // Check MARK button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class = "fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    // Check trash BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class = "fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+    // APPEND TO LIST
+    todoList.appendChild(todoDiv);
+
 }
