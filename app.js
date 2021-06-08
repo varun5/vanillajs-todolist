2433db47a866510e0b3223d6702d31543435fbf4
Varun Nayak <varunnayak@Varuns-MacBook-Pro.local>
2021-06-07 20:43:33 -0500

    Added buttons

---
+++ app.js
@@ -0,0 +1,10 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList= document.querySelector('.todo-list');
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+//Functions
+function addTodo(event) {
+    console.log("hello");
+}
